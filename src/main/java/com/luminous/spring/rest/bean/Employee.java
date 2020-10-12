3e112e852795885766075775b1ef2c794db726f7
akirachr <akirachr@gmail.com>
2020-10-12 15:55:59 +0900

    Add files via upload

---
+++ src/main/java/com/luminous/spring/rest/bean/Employee.java
@@ -0,0 +1,35 @@
+package com.luminous.spring.rest.bean;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Employee {
+	private @Id @GeneratedValue Long id;
+	private String name;
+
+	public Employee() {
+	}
+	
+	public Employee(String name) {
+		setName(name);
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
