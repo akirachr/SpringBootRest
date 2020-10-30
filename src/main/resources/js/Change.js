1f4c147da6abc067c891a1b5adef1fb8d6b88d90
akirachr <akirachr@gmail.com>
2020-10-30 11:54:21 +0900

    Add files via upload

---
+++ src/main/resources/js/Change.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+
+class Change extends Component{
+
+    render(){
+      return(
+        <Container>
+          Change
+          </Container>
+        )
+    }
+}
+
+
+export default Change;
---
+++ src/main/resources/js/Create.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+
+class Create extends Component{
+
+    render(){
+      return(
+        <Container>
+          Create
+          </Container>
+        )
+    }
+}
+
+
+export default Create;
